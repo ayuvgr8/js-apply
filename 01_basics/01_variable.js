1d91009051b9b288020e5622cb748c6794040104
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2024-01-18 18:15:10 +0000

    Font

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144555 // not going to change 
+let accountEmail = "ayuvgr8@gmail.com"
+var accountPassword = "23456werty"
+accountCity = "Gurugram"
+
+//accountId = 6789 // not allowed 
+accountEmail ="asdfg@gamil.com"
+accountPassword = "2asdfg345"
+accountCity = "Lucknow"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
