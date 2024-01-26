1b0689bc09a8dfd7dd87ad032f3937b547db7703
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2024-01-26 16:51:47 +0000

    datattypes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,7 +1,11 @@
 const accountId = 144555 // not going to change 
 let accountEmail = "ayuvgr8@gmail.com"
 var accountPassword = "23456werty"
-accountCity = "Gurugram"
+accountCity = "Gurugram" // -> aise bhi declare hota hai
+
+let accountState; // Semi colon lagao ya nna lago aapki marzi
+
+
 
 //accountId = 6789 // not allowed 
 accountEmail ="asdfg@gamil.com"
@@ -9,4 +13,10 @@ accountPassword = "2asdfg345"
 accountCity = "Lucknow"
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+prefer not to use var
+bcoz issue i block scope and functioal scope
+*/
+
---
+++ 01_basics/02_datatTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code in newer version
+
+//alert(3 +3) // we are using node.js not browser
+console.log(3+3)
+
+// mdn and. tc39.es // ECMA Script
+
+let name = "Ayush"
+let age = 24
+let isLoggedIn = true
+let state =null;
+
+//    number -> 2 to tthe power 53
+//    bigInt ->  use in trading like apps
+//.   string -> " "
+//.   boolean -> true/false
+//.   null -> standalone value -> represetatio of empty value 
+//.   undefined ->  variable is defined but value is not assigned
+//.   symbol  -> unique
+
+
+//object
+
+
+console.log(typeof null);
+console.log(typeof undefined);
